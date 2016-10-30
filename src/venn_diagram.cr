7abb052353b39bf9682503f02e31e79ebb351194
Peter Tseng <pht24@cornell.edu>
2016-10-29 19:18:09 -0700

    use UInt32
    The probabilities can't be negative, nor can the region numbers. This is
    slightly less ergonomic since it needs _u32 suffixes in places, but I
    want it anyway.

--- src/venn_diagram.cr
+++ src/venn_diagram.cr
@@ -4,7 +4,7 @@
 # The bit pattern of region R tells the truth value of the variables at R.
 # For example, with the three variables above, region 6 (110) is C true, B true, A false
 class VennDiagram
-  CERTAINTY = 100
+  CERTAINTY = 100_u32
 
   @var_indices : Hash(String, Int32)
 
@@ -19,41 +19,39 @@ class VennDiagram
   #
   # This should tell us our memory requirement is 2**(2**N)
   # e.g. with 3 variables we have 8 regions and 128 subsets of those regions.
-  #
-  # !!! Again, I'd like to use UInt32 for this, but Array#size is an Int32
-  @knowns : Hash(Set(Int32), Int32)
+  @knowns : Hash(Set(UInt32), UInt32)
 
   def initialize(@vars : Array(String))
     @var_indices = @vars.each_with_index.to_h
-    all_regions = (0...size).to_set
+    all_regions = (0_u32...size).to_set
     @knowns = {all_regions => CERTAINTY}
   end
 
-  def size
-    2 ** @vars.size
+  def size : UInt32
+    2_u32 ** @vars.size
   end
 
   def learn(fact : String)
     vars, probability = fact.split(": ")
-    learn_and_infer(regions_of(vars), probability.to_i, "GIVEN")
+    learn_and_infer(regions_of(vars), probability.to_u32, "GIVEN")
   end
 
-  def query(question : String) : Int32?
+  def query(question : String) : UInt32?
     puts "I now know #{@knowns.size} region-sets"
     @knowns[regions_of(question)]?
   end
 
-  def regions_of(vars : String) : Set(Int32)
+  def regions_of(vars : String) : Set(UInt32)
     vars = vars.split(" & ")
     known_values = vars.map { |v|
       v[0] == '!' ? {false, @var_indices[v[1..-1]]} : {true, @var_indices[v]}
     }
-    known_portion = known_values.map { |on, pos| (on ? 1 : 0) << pos }.reduce(0) { |a, b| a | b }
+    known_portion = known_values.map { |on, pos| (on ? 1_u32 : 0_u32) << pos }.reduce(0_u32) { |a, b| a | b }
     unknown_bits = (0...@vars.size).to_set - known_values.map { |_, pos| pos }.to_set
-    regions = unknown_bits.reduce([known_portion]) { |regs, bit| regs.flat_map { |r| [r, r | (1 << bit)] } }.to_set
+    regions = unknown_bits.reduce([known_portion]) { |regs, bit| regs.flat_map { |r| [r, r | (1_u32 << bit)] } }.to_set
   end
 
-  private def learn_and_infer(regions : Set(Int32), probability : Int32, comment : String)
+  private def learn_and_infer(regions : Set(UInt32), probability : UInt32, comment : String)
     if @knowns[regions]?
       raise "At #{regions}, known value #{@knowns[regions]} conflicts with new value of #{probability}" if @knowns[regions] != probability
       return
